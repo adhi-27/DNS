fda2dfeaf99b1faaec8e417cd70d551cdfd14358
adhi-27 <adhityasutresh59@gmail.com>
2020-02-01 20:24:35 +0530

    DNS Server

--- lookup.rb
+++ lookup.rb
@@ -18,34 +18,35 @@ domain = get_command_line_argument
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-def parse_dns(dns_zone)
-	records={}
-	dns_zone.each do |rec|
-			rec_split=rec.split(", ")
-			if records[rec_split[0]].nil?
-				records[rec_split[0]]={ rec_split[1] => rec_split[2].chomp }
-			else
-				records[rec_split[0]][rec_split[1]] = rec_split[2].chomp
-			end
-	end
-	records
+def parse_dns(raw)
+	domains={}
+	raw.
+		reject {|record| record.empty? }.
+		reject {|record| record[0] == "#" }.
+		each do |record|
+			rec_split=record.strip.split(", ")
+			domains[rec_split[1]] = { rec_split[0] => rec_split[2] }
+		end
+		domains
 end
 
 def resolve(dns_records, lookup_chain, domain)
-	dns_records.each do |key_type,record|
-		record.each do |key,next_domain|
-			if key==domain
-				if key_type=="A"
-					lookup_chain+=[next_domain]
-				elsif key_type=="CNAME"
-					lookup_chain+=[next_domain]
-					lookup_chain=resolve(dns_records, lookup_chain, next_domain)
-				end
-				return lookup_chain
+	record = dns_records[domain]
+	if (!record)
+		lookup_chain += ["Error: Domain Not In Record"]
+	else
+		record.each do |key, next_domain|
+			if key == "A"
+				lookup_chain += [next_domain]
+			elsif key == "CNAME"
+				lookup_chain += [next_domain]
+				lookup_chain = resolve(dns_records, lookup_chain, next_domain)
+			else
+				lookup_chain += ["Invalid Record Type for "+domain]
 			end
 		end
 	end
-	return lookup_chain+=["Domain Name not Found"]
+	lookup_chain
 end
 
 # To complete the assignment, implement `parse_dns` and `resolve`.
